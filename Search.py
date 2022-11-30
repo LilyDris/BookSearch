2bc3d69ae8ac796b772e85739bc0aea21293d7c3
Driscoll <Lily.Driscoll@franklincountyohio.gov>
2022-11-30 12:38:19 -0500

    Start of program

--- Search.py
+++ Search.py
@@ -23,6 +23,8 @@ def main():
     driver = webdriver.Chrome('chromedriver')# need to updatee chromedriver version
     driver.get("https://www.barnesandnoble.com") # Change depending on server
     wait = np.random.uniform(low=1.0, high=2.0, size=50)
+    driver.find_element(by=By.XPATH, value='/html/body/div[2]/header/nav/div/div[3]/form/div/div[2]/div/input[1]').send_keys('Sarah')
+    
     
 if __name__ == "__main__":
     main()
